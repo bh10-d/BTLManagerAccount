48a09385a2817e151e981ba6ced7325d1b7f330a
Administrator <Administrator@HieuBui>
2021-01-17 22:35:20 +0700

    manageraccount khoi chay screentwo

--- src/ManagerAccount.java
+++ src/ManagerAccount.java
@@ -1,54 +1,10 @@
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.*;
-
 public class ManagerAccount{
 	public static void main(String[]args) {
 		
-		 JFrame frame = new JFrame();
-		
-		 JPanel basic = new JPanel();
-		 // tao 1 panel theo chieu tu tren xuong//
-		 basic.setLayout(new BoxLayout(basic, BoxLayout.Y_AXIS));
-		 frame.add(basic);
-		 //tao 1 panel theo chieu ngang va ep sang phai//
-		 basic.add(Box.createVerticalGlue());
-		 
-		 JPanel bottom = new JPanel();
-		 bottom.setAlignmentX(Component.RIGHT_ALIGNMENT);
-	     bottom.setLayout(new BoxLayout(bottom, BoxLayout.X_AXIS));
-	     
-	     JButton Add = new JButton("Add");
-	     JButton Change = new JButton("Change");
-	     JButton Delete = new JButton("Delete");
-	 
-	     
-	     bottom.add(Add);
-	     bottom.add(Box.createRigidArea(new Dimension(60,0)));
-	     
-	     bottom.add(Change);
-	     bottom.add(Box.createRigidArea(new Dimension(60, 0)));
-	     bottom.add(Delete);
-	     bottom.add(Box.createRigidArea(new Dimension(15, 0)));
-	 
-	     basic.add(bottom);
-	     basic.add(Box.createRigidArea(new Dimension(0, 15)));
-	     //
-	     
+		Screentwo sc = new Screentwo();
+		sc.Screen();
 
-	     
-	 
-	     
-	     //container
-	     JLabel label = new JLabel("First App"); 
-	     frame.setMinimumSize(new Dimension(900,600));
-	     frame.setMaximumSize(new Dimension(900,600));
-	     frame.setVisible(true);
-		 
-		 
 		 
 	}
 }
---
+++ src/Screentwo.java
@@ -0,0 +1,115 @@
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.*;
+
+
+public class Screentwo extends Frame{
+	
+	private JFrame frame;
+	private JPanel panel;
+
+	
+//	public Screentwo() {
+//		Sct();
+//	}
+
+
+	public void Screen() {
+		
+		frame = new JFrame("Account Manager");
+		setMinimumSize(new Dimension(900, 600));
+		setMaximumSize(new Dimension(900, 600));
+		
+		panel = new JPanel();
+		panel.setBounds(450, 0, 450, 600);
+		panel.setBackground(Color.gray);
+		
+
+		JList listtt = new JList();
+		listtt.setFont(new Font("Calibri", Font.BOLD, 15));
+		
+		
+		JLabel yourmoney = new JLabel("Enter Money");
+		yourmoney.setFont(new Font("Calibri",Font.BOLD,30));
+		JLabel date = new JLabel("Date");
+		date.setFont(new Font("Calibri",Font.BOLD,30));
+		JLabel des = new JLabel("Description");
+		des.setFont(new Font("Calibri",Font.BOLD,30));
+		JLabel sum = new JLabel("Account Balance = ");
+		sum.setFont(new Font("Calibri",Font.BOLD,25));
+		JLabel list = new JLabel("List");
+		list.setFont(new Font("Calibri", Font.BOLD, 40));
+		
+		
+		JButton Add = new JButton("Add");
+		JButton Charts = new JButton("Charts");
+		JButton Sub = new JButton("Sub");
+		
+		
+		JTextField moneyt = new JTextField();
+		moneyt.setFont(new Font("Calibri", Font.BOLD, 30));
+		JTextField datet = new JTextField();
+		datet.setFont(new Font("Calibri", Font.BOLD, 30));
+		JTextField dest = new JTextField();
+		dest.setFont(new Font("Calibri", Font.BOLD, 20));
+		
+		
+		JTextArea sumt = new JTextArea();
+		sumt.setFont(new Font("Calibri", Font.BOLD, 30));
+//		JTextArea listt = new JTextArea();
+//		listt.setFont(new Font("Calibri", Font.BOLD, 15));
+
+		frame.setMinimumSize(new Dimension(900,600));
+		frame.setMaximumSize(new Dimension(900,600));
+		
+
+		yourmoney.setBounds(500, 50, 200,30);
+		date.setBounds(500, 200, 200, 30);
+		des.setBounds(500, 350, 200, 30);
+		sum.setBounds(10, 480, 250, 60);
+		list.setBounds(40, 0, 100, 50);
+		listtt.setBounds(40,40,370,420);
+		
+		Add.setBounds(500, 500, 90, 30);
+		Charts.setBounds(740, 500, 90, 30);
+		Sub.setBounds(620, 500, 90, 30);
+		
+		
+		moneyt.setBounds(500, 100, 300, 50);
+		datet.setBounds(500, 250, 300, 50);
+		dest.setBounds(500, 400, 300, 50);
+		sumt.setBounds(200, 490, 200, 30);// textArea nay se tinh tong so tien hien co!!!
+
+		
+		frame.add(yourmoney);
+		frame.add(date);
+		frame.add(des);
+		frame.add(sum);
+		frame.add(list);
+		frame.add(Add);
+		frame.add(Charts);
+		frame.add(Sub);
+		frame.add(moneyt);
+		frame.add(datet);
+		frame.add(dest);
+		frame.add(sumt);
+		frame.add(listtt);
+		
+		
+		frame.add(panel);
+		frame.setLayout(null);
+		
+		
+		
+		
+		frame.setVisible(true);
+		
+
+		
+	}
+	
+	
+}
