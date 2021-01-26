c5f22274564d49192282dba91aa73a2d0639e99e
bh10-d <duchieubui25012002@gmail.com>
2021-01-26 19:16:48 +0700

    hoan thien jlist bo sung lop control

--- src/PackageController/ControllCenter.java
+++ src/PackageController/ControllCenter.java
@@ -1,49 +1,54 @@
 package PackageController;
 
-import PackageModel.*;
-import PackageView.*;
-import PackageController.*;
-
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.*;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
-//import Account;
-//import Dates;
-//import Screentwo;
-import PackageModel.*;
+import javax.swing.ListModel;
 
 import java.util.*;
 
 public class ControllCenter {
 	storeData2 storeAccount;
-	//ahihi
+	private String textYourMoney;
+	private long longYourMoney;
+	private String textDate;
+	private Dates YourDate;
+	private String[] arrTextDate;
+	private String textDes;
+	private Account account;
+	private ArrayList<Account> temporaryStorage;
+	private String textLabels;
 	
 	public ControllCenter() {
+		Control Ctrl = new Control();
 		Screentwo.Screen();
-		//Centre center2 = new Centre();
-		
+		DefaultListModel<Control> mode = new DefaultListModel<>();// set mode cho list 
+		Screentwo.getListt().setModel(mode);//_____________________________
+		// Tao su kien cho button Add(Them,Cong)
 		Screentwo.getAdd().addActionListener(new ActionListener() {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				String textYourMoney = Screentwo.getMoneyt().getText();
+//_________________________________________________________________________________________//
+				// Xu ly tien
+				textYourMoney = Screentwo.getMoneyt().getText();
 				Account.setMoneyFlus(Long.parseLong(textYourMoney));
-				long longYourMoney = Account.getMoney();
-				
-				String textDate = Screentwo.getDatet().getText();
-				
-				String[] arrTextDate = textDate.split("/");
-				Dates YourDate = new Dates(Integer.parseInt(arrTextDate[0]),
+				longYourMoney = Account.getMoney();
+				// Xu ly ngay
+				textDate = Screentwo.getDatet().getText();
+				arrTextDate = textDate.split("/");
+				YourDate = new Dates(Integer.parseInt(arrTextDate[0]),
 						Integer.parseInt(arrTextDate[1]),Integer.parseInt(arrTextDate[2]));
-				
-				String textDes = Screentwo.getDest().getText();
-				
+				// Xu ly mo ta
+				textDes = Screentwo.getDest().getText();
+//_________________________________________________________________________________________//
 				/*
 				getGUI_ja userVsPasswordOfYour = new getGUI_ja();
 				String userVaPassword = userVsPasswordOfYour.toString();
@@ -51,27 +56,37 @@ public class ControllCenter {
 				*/
 				//cai ni de luu tru tam thoi chuan bi luu vo arraylist(luu vo array tam)
 				
-				Account account = new Account(longYourMoney,textDes,YourDate);
-				ArrayList<Account> temporaryStorage = new ArrayList<Account>();
+				account = new Account(longYourMoney,textDes,YourDate);
+				temporaryStorage = new ArrayList<Account>();
 				temporaryStorage.add(account);
+				
 				//luu vo arraylist toan cuc(luu tru ben lop storeData)
+				
 				storeAccount = new storeData2(temporaryStorage);
+				// Dua du lieu vao 
+				Ctrl.setTextDate(textDate);
+				Ctrl.setTextDes(textDes);
+//_________________________________________________________________________________________//
+				JTextArea/*JLabel*/ textLabels = new JTextArea();
+				String textdate = new String();
+				JTextArea textmoney = new JTextArea();
+				String money = new String();
+				money = Long.toString(Account.getMoney());
+				textdate = YourDate.toString();
 				
+				// Dua du lieu vao
+				Ctrl.setTextmoney(money);
+				// Lay du lieu ra va in ra man hinh
+				textLabels.setText(Ctrl.getTextDate()+Ctrl.getTextDes()+Ctrl.getTextmoney());
+				mode.addElement(new Control(Ctrl.getTextDate(),Ctrl.getTextDes(),Ctrl.getTextmoney()));
 				
-				JLabel textLabels = new JLabel();
-				//Account.setMoneyFlus(longYourMoney);
-				
-				textLabels.setText("Thoi gian :"+YourDate.toString()+", Description :"+textDes+", money:"+Long.toString(Account.getMoney()));
-				Screentwo.getPanel().add(textLabels);
-				
-				
-				
-				Screentwo.getSumt().setText(Long.toString(Account.getMoney()));
+				Screentwo.getSumt().setText(Long.toString(Account.getMoney()));// in ra tong tien 
 				Screentwo.getPanel().setVisible(true);
 			}
 	
 		});
 		
+		// Tao su kien cho button Sub(Tru)
 		Screentwo.getSub().addActionListener(new ActionListener() {
 			
 			@Override
@@ -79,15 +94,13 @@ public class ControllCenter {
 				String textYourMoney = Screentwo.getMoneyt().getText();
 				Account.setMoneySub(Long.parseLong(textYourMoney));
 				long longYourMoney = Account.getMoney();
-				
 				String textDate = Screentwo.getDatet().getText();
-				
 				String[] arrTextDate = textDate.split("/");
 				Dates YourDate = new Dates(Integer.parseInt(arrTextDate[0]),
 						Integer.parseInt(arrTextDate[1]),Integer.parseInt(arrTextDate[2]));
-				
+				// Lay du lieu cua muc mo ta
 				String textDes = Screentwo.getDest().getText();
-				
+//_________________________________________________________________________________________//				
 				/*
 				getGUI_ja userVsPasswordOfYour = new getGUI_ja();
 				String userVaPassword = userVsPasswordOfYour.toString();
@@ -100,22 +113,35 @@ public class ControllCenter {
 				temporaryStorage.add(account);
 				//luu vo arraylist toan cuc(luu tru ben lop storeData)
 				storeAccount = new storeData2(temporaryStorage);
+//_________________________________________________________________________________________//
+				JTextArea textLabels = new JTextArea();
+				//JTextArea textmoney = new JTextArea();
+				String money = new String();
+				money = Long.toString(Account.getMoney());
+				// Dua du lieu vao
+				Ctrl.setTextmoney(money);
+				Ctrl.setTextDate(textDate);
+				Ctrl.setTextDes(textDes);
+				// Lay du lieu ra va in ra man hinh
+				textLabels.setText(Ctrl.getTextDate()+Ctrl.getTextDes()+Ctrl.getTextmoney());
+				mode.addElement(new Control(Ctrl.getTextDate(),Ctrl.getTextDes(),Ctrl.getTextmoney()));
 				
-				
-				JLabel textLabels = new JLabel();
-				//Account.setMoneyFlus(longYourMoney);
-				
-				textLabels.setText("Thoi gian :"+YourDate.toString()+", Description :"+textDes+", money:"+Long.toString(Account.getMoney()));
-				Screentwo.getPanel().add(textLabels);
-				
-				
-				
-				Screentwo.getSumt().setText(Long.toString(Account.getMoney()));
+//_________________________________________________________________________________________//				
+				//Screentwo.getScrollPane().add(textmoney);
+				Screentwo.getSumt().setText(Long.toString(Account.getMoney()));// lay tong tien ra man hinh
 				Screentwo.getPanel().setVisible(true);
 				
 			}
 		});
 		
+
+		
+		
+		
+		
+		
+		
+		// chuc nang charts
 		Screentwo.getCharts().addActionListener(new ActionListener() {
 			
 			@Override
@@ -127,4 +153,6 @@ public class ControllCenter {
 		});
 		
 	}
+
 }
+
